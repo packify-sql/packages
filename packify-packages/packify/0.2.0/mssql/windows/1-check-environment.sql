fa268552074bfc31bc550b44a586524862d39911
Will Hinson <wmhcell@gmail.com>
2024-06-26 11:10:49 -0400

    Implemented 1-check-environment.sql

--- packify-packages/packify/0.2.0/mssql/windows/1-check-environment.sql
+++ packify-packages/packify/0.2.0/mssql/windows/1-check-environment.sql
@@ -1,1 +1,55 @@
-PRINT 'would check for :database';
+/*
+ * 1-check-environment.sql
+ *
+ * Checks that the environment allows for Packify to be installed
+ * as configured. For example, the target database should not already
+ * exist and we should be able to create a login for Packify
+ */
+
+DECLARE
+    @errorNumber        INT,
+    @errorMessage       NVARCHAR(200);
+
+PRINT 'Check environment for installation';
+
+/* Check that the target database doesn't exist */
+IF EXISTS (
+    SELECT
+        *
+    FROM
+        sys.databases
+    WHERE
+        [name] = ':database_unescaped'
+) BEGIN
+    SET @errorNumber = 98000;
+    SET @errorMessage = 'Target database :database_escaped already exists';
+
+    GOTO Error;
+END
+
+PRINT 'PASS: Target database :database_escaped does not exist';
+
+/* Check that the PackifyLogin doesn't already exist */
+IF EXISTS (
+    SELECT
+        *
+    FROM
+        sys.syslogins
+    WHERE
+        [name] = 'PackifyLogin'
+) BEGIN
+    SET @errorNumber = 98010;
+    SET @errorMessage = 'Target login PackifyLogin already exists';
+
+    GOTO Error;
+END
+
+PRINT 'PASS: Target login PackifyLogin does not exist';
+
+RETURN;
+
+Error:
+    THROW
+        @errorNumber,
+        @errorMessage,
+        1;
